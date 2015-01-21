9464a0abe804f0d400ff3aba9b08331ad302ac1f
thisGit <thisGit@gmail.com>
2015-01-21 23:32:49 +0100

    Cleaning

--- FiboEven.scala
+++ FiboEven.scala
@@ -3,14 +3,21 @@ package project.euler
 object FiboEven {
 
   def main(args : Array[String]): Unit = {
-    Console println fiboEven()
+    Console println fiboEven
   }
+  
+  /**
+   *
+   */
   def fiboEven(): Int = {                                
-    def fibonacci(f: Int, n: Int, acc: Int): Int = {
+    /**
+     *
+     */
+    def fibonacci(f: Int, n: Int, acc: Int): Int =
       if (n > 4000000) acc
       else if (n % 2 == 0) fibonacci(n, f+n, acc+n)
       else fibonacci(n, f+n, acc)  
-    }                                                     
+
     fibonacci(1, 2, 0)                                    
   }
 }
