6cf98be2724a9fe9a109a56cadf423bb0863f692
Estarb <39631@heb.be>
2015-01-22 08:43:48 +0100

    Cleaning

--- Mult.scala
+++ Mult.scala
@@ -1,17 +1,16 @@
 package project.euler
 
 object Mult {
-  def main(args : Array[String]) = {
-    for (i <- 1 to 10)
+  def main(args : Array[String]) = 
+    for (i <- 1 to 10) 
       Console println mul(i)
-  }
 
-  def mul(n : Int): Int = {                              
-    def mulRec(a : Int, acc: Int): Int = {                 
+  def mul(n : Int): Int = {
+    def mulRec(a : Int, acc: Int): Int =
       if(a >= n) acc                                        
-      else if (a%3==0 || a%5==0) mulRec(a+1, acc+a)         
-      else mulRec(a+1, acc)                                 
-    }                                                      
+      else if (a % 3 == 0 || a % 5 == 0) mulRec(a + 1, acc + a)         
+      else mulRec(a + 1, acc) 
+                                      
     mulRec(1, 0)                                           
   }  
 }
