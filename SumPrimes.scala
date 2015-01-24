2a513c0187e13f27f1139d9fa40733ed89acdffe
Estarb <39631@heb.be>
2015-01-24 14:54:57 +0100

    WIP

--- SumPrimes.scala
+++ SumPrimes.scala
@@ -13,16 +13,19 @@ object SumPrimes {
 
     @tailrec def primesRec(a: BigInt, primes: List[BigInt]): List[BigInt] =
       if (a * a >= n) primes
-      else if (primes.forall(x => a % x != 0)) primesRec(a + 1, a :: primes)
-      else primesRec(a + 1, primes)
+      else if (primes.forall(x => a % x != 0)) primesRec(a + 2, a :: primes)
+      else primesRec(a + 2, primes)
 
-    @tailrec def rmPrimesRec(t: BigInt, p: List[BigInt], numbers: List[BigInt]):
-    List[BigInt] = 
-      if (p == Nil) numbers
+    @tailrec def rmPrimesRec(t: BigInt, p: List[BigInt], numbers: List[BigInt]): List[BigInt] = 
+      if (p == Nil) {println(numbers); numbers}
       else if (t * p.head > numbers.head) rmPrimesRec(2, p.tail, numbers)
       else rmPrimesRec(t + 1, p, numbers diff List[BigInt](t * p.head))
-    
-    rmPrimesRec(2, primesRec(2, Nil), (n to BigInt(2) by -1).toList).sum
 
+      if (n % 2 == 0)
+        rmPrimesRec(2, primesRec(3, List[BigInt](2)), ((n - 1) to BigInt(2) by
+          -2).toList ++ List[BigInt](2)).sum
+      else
+        rmPrimesRec(2, primesRec(3, List[BigInt](2)), (n to BigInt(2) by
+          -2).toList ++ List[BigInt](2)).sum
   }
 }
