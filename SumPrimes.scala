9debe6492c63dd6a673c194244b7b3fc4334f2e0
Estarb <39631@heb.be>
2015-01-24 13:31:20 +0100

    #10 probably done but do not have enough memory for 2 000 000

--- SumPrimes.scala
+++ SumPrimes.scala
@@ -16,10 +16,13 @@ object SumPrimes {
       else if (primes.forall(x => a % x != 0)) primesRec(a + 1, a :: primes)
       else primesRec(a + 1, primes)
 
-    @tailrec def rmPrimesRec(t: BigInt, b: BigInt, numbers: List[BigInt]): List[BigInt] =
-      if (t * b > n) numbers
-      else rmPrimesRec(t + 1, b, numbers diff List[BigInt](t * b))
-
+    @tailrec def rmPrimesRec(t: BigInt, p: List[BigInt], numbers: List[BigInt]):
+    List[BigInt] = 
+      if (p == Nil) numbers
+      else if (t * p.head > numbers.head) rmPrimesRec(2, p.tail, numbers)
+      else rmPrimesRec(t + 1, p, numbers diff List[BigInt](t * p.head))
+    
+    rmPrimesRec(2, primesRec(2, Nil), (n to BigInt(2) by -1).toList).sum
 
   }
 }
